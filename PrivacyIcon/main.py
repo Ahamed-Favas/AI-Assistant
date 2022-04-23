665dbc191dadcbc4fd3b8e21e0a0e05198ecf75c
arjunvtsuresh <arjunvtsuresh@gmail.com>
2022-04-23 13:22:38 +0530

    added Privacy Icon

---
+++ PrivacyIcon/main.py
@@ -0,0 +1,19 @@
+from tkinter import *
+from PIL import ImageTk , Image
+import time
+win = Tk()
+win.title(None)
+win.geometry('80x75+1830+900')
+
+#canvas
+canvas = Canvas(win , width = 80 , height=75)
+canvas.pack()
+
+#animate
+img = ImageTk.PhotoImage(Image.open("mic1.jpg"))
+canvas.create_image(1,1,anchor=NW,image=img)
+win.wm_attributes('-topmost','True')
+
+##only on windows
+##win.wm_attributes('-toolwindow','True')
+win.mainloop()
